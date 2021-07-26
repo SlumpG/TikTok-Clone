1a01e7b5557d399e8d91e48a30ae399f98e43d15
SlumpG <shlomo.gangul1@gmail.com>
2021-07-26 10:42:35 +0300

    deploed to firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-
-import './App.css';
-import Video from './Components/Video/Video';
+import axios from "./FetchData/axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Video from "./Components/Video/Video";
 
 function App() {
+
+  const [videos,setVideos] = useState([])
+useEffect(()=>{
+  async function fetchPosts(){
+    const response = await axios.get("/v2/posts")
+    setVideos(response.data)
+    return response
+  }
+  fetchPosts()
+
+},[])
+
   return (
     <div className="app">
-    <div className="app_videos">
-
-      <Video color={'greenyellow'} channel={'user1'} discription={'beastUser1 mode'} song={'Kendrik Lamar - YAK YAK YAK'} likes={485} messages={321} shares={55} />
-      <Video color={'red'} channel={'user2'} discription={'beastUser2 mode'} song={'J Cole - G.O.D'} likes={925} messages={351} shares={5} />
-    </div>
+      <div className="app_videos">
+        {
+          videos.map((video) =>{
+            return <Video
+            color={"greenyellow"}
+            url={"https://www.tiktok.com/@elcachorrojr16/video/6976500925527608581?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6988820333768951297"}
+            channel={video.channel}
+            discription={video.discription}
+            song={video.song}
+            likes={video.likes}
+            messages={video.messages}
+            shares={video.shares}
+          />
+          })
+        }
+     
+       
+    
+      </div>
     </div>
   );
 }
